f3cccc7b9e28a711e50d7f0864b51a9e66749295
renpeng.liu <renpeng.liu@yff.com>
2017-08-02 11:39:49 +0800

    8.1

--- node/gl.js
+++ node/gl.js
@@ -1,7 +1,13 @@
-module.exports.a=1;
-console.log(__filename);
-console.log(__dirname)
-console.log(module)
-console.log(process.title)
-console.log(process.arch)
-console.log(process.memoryUsage())
+// module.exports.a=1;
+// console.log(__filename);
+// console.log(__dirname)
+// console.log(module)
+// console.log(process.title)
+// console.log(process.arch)
+// console.log(process.memoryUsage())
+console.log(null == null )
+console.log(undefined == undefined)
+console.log(null === null)
+console.log(undefined === undefined)
+console.log(null === undefined)
+console.log(null===undefined)
